0a4f34269a22d2fe8083c6fdea88e08a44ca8737
EF <eorf256@gmail.com>
2024-09-25 02:44:10 +0900

    タスクチュートリアル

---
+++ TodoItem.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorTutorial
+{
+    public enum Status
+    {
+        NotStarted,
+        InProgress,
+        Completed
+    }
+    
+    public class TodoItem
+    {
+        public string? Title { get; set; }
+        public bool IsDone { get; set; }
+        public Status Status { get; set; }
+    }
+}
